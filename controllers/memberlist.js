32f71d63f4544ab5821148086ac5aec195cbf7a3
Swardiantara Silalahi <swardyantara@gmail.com>
2020-11-11 07:23:53 +0700

    memberlist dikit lagi

--- controllers/memberlist.js
+++ controllers/memberlist.js
@@ -1,15 +1,34 @@
 const User = require("../models/user");
+const moment = require('moment');
 
-exports.getMemberlist = (req, res) => {
-  User.find().populate('license').then((data) => {
-    data = data.filter((item, index) => {
-      return item.role == 'Member'
-    })
-
-    return res.render("memberlist/index", {
-      title: "Leads Panel",
-      data: data,
-      user: req.user,
-    });
+exports.getMemberlist = async (req, res) => {
+  moment.locale('ID');
+  let user = await User.find().populate('downline').populate('comission').populate('license');
+  user = user.filter((item, index) => {
+    return item.role == 'Member'
+  })
+  user.map((item, index) => {
+    // item.license.some(license => {
+    //   return license.name == 'Premium'
+    // })
+    // console.log(item.license)
+    item.license = item.license.some(license => {
+      return license.name == 'Premium'
+    }) ? 'Premium' : 'Basic';
+  })
+  user.map((item, index) => {
+    let total = item.commission.reduce((prev, cur) => {
+      return cur.status == 'not_paid' ? prev + cur.jumlah : prev + 0;
+    }, 0)
+    item.komisi = total;
+    console.log(item.komisi);
+  })
+  // let total = user.commission.reduce((prev, cur) => {
+  //   return cur.status == 'not_paid' ? prev + cur.jumlah : prev + 0;
+  // }, 0)
+  return res.render("memberlist/index", {
+    title: "Leads Panel",
+    data: user,
+    user: req.user,
   });
 };
